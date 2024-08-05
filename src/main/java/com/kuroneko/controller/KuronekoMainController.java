d00a2064e94d50df7eb9ca40eb7f182dc1abf532
Hubkaw <2137insidejob@gmail.com>
2024-08-05 14:17:24 +0200

    fix whitelabel on wrong link

--- src/main/java/com/kuroneko/controller/KuronekoMainController.java
+++ src/main/java/com/kuroneko/controller/KuronekoMainController.java
@@ -3,13 +3,15 @@ package com.kuroneko.controller;
 import lombok.AllArgsConstructor;
 import net.dv8tion.jda.api.JDA;
 import net.dv8tion.jda.api.Permission;
+import org.springframework.boot.web.servlet.error.ErrorController;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @AllArgsConstructor
-public class KuronekoMainController {
+public class KuronekoMainController implements ErrorController {
 
     private JDA JDA;
 
@@ -19,4 +21,10 @@ public class KuronekoMainController {
         model.addAttribute("kuronekoInvite", JDA.getInviteUrl(Permission.ADMINISTRATOR));
         return "main";
     }
+
+    @RequestMapping("/error")
+    public String getKuronekoErrorPage(Model model){
+        return "redirect:/";
+    }
+
 }
