55fef4d1b3b4dd0fabe5ddb9b57e922be9c87e7c
Dawid Zgoda <acc0rd.2goda@gmail.com>
2025-03-03 10:29:33 +0100

    ENV Support

--- src/main/java/com/kuroneko/config/ConfigLoader.java
+++ src/main/java/com/kuroneko/config/ConfigLoader.java
@@ -1,6 +1,7 @@
 package com.kuroneko.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import io.github.cdimascio.dotenv.Dotenv;
 import org.springframework.core.io.FileSystemResource;
 
 import java.nio.file.Path;
@@ -10,12 +11,33 @@ import java.nio.file.Paths;
 public class ConfigLoader {
 
     private static Config config;
-
+    private static Dotenv ENV;
+    ConfigLoader(Dotenv env) {
+        ENV = env;
+    }
     public static Config getConfig(){
         if (config == null){
-            config = configLoader();
+            checkAndLoadConfig();
         }
         return config;
+    }
+    private static void checkAndLoadConfig(){
+        Path p = Paths.get(System.getProperty("user.home"), "kuroneko_config.json");
+        if(p.toFile().exists()){
+           config = configLoader();
+        }else{
+          generateConfig();
+
+        }
+    }
+
+    private static void generateConfig() {
+        config = new Config();
+        config.setDbUser(ENV.get("DB_USER"));
+        config.setDbPassword(ENV.get("DB_PASSWORD"));
+        config.setDiscordToken(ENV.get("DISCORD_TOKEN"));
+        config.setRiotToken(ENV.get("ROIT_TOKEN"));
+        config.setYoutubeToken(ENV.get("YOUTUBE_TOKEN"));
     }
 
     private static Config configLoader() {
---
+++ src/main/java/com/kuroneko/config/EnvLoader.java
@@ -0,0 +1,26 @@
+package com.kuroneko.config;
+
+import io.github.cdimascio.dotenv.Dotenv;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
+@Configuration
+public class EnvLoader {
+
+    @Bean
+    public static Dotenv Env() {
+        Path envPath = Paths.get(".env");
+        if(Files.exists(envPath)) {
+            System.out.println(".env loaded");
+            return Dotenv.load();
+        }
+        else{
+            System.err.println(".env not loaded");
+            return null;
+        }
+    }
+}
