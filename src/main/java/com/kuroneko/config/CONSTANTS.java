a9fb338620d5c4900ea1d413e2f6efef3cc1ccd2
Hubkaw <2137insidejob@gmail.com>
2025-08-07 15:24:47 +0200

    Support exploding dice

---
+++ src/main/java/com/kuroneko/config/CONSTANTS.java
@@ -0,0 +1,8 @@
+package com.kuroneko.config;
+
+public class CONSTANTS {
+    public static final int LOWEST_DICE_VALUE = 2;
+    public static final int HIGHEST_DICE_VALUE = 10000;
+    public static final int LOWEST_DICE_AMOUNT = 1;
+    public static final int HIGHEST_DICE_AMOUNT = 100;
+}
