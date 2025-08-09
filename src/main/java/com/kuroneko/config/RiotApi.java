b111ac7b835fc7f6eb09586189deaa38f8a70f65
Hubkaw <2137insidejob@gmail.com>
2025-08-09 16:11:27 +0200

    set r4j cache provider to EmptyCacheProvider

--- src/main/java/com/kuroneko/config/RiotApi.java
+++ src/main/java/com/kuroneko/config/RiotApi.java
@@ -1,6 +1,8 @@
 package com.kuroneko.config;
 
 import no.stelar7.api.r4j.basic.APICredentials;
+import no.stelar7.api.r4j.basic.cache.impl.EmptyCacheProvider;
+import no.stelar7.api.r4j.basic.calling.DataCall;
 import no.stelar7.api.r4j.impl.R4J;
 import org.springframework.stereotype.Component;
 
@@ -8,5 +10,6 @@ import org.springframework.stereotype.Component;
 public class RiotApi extends R4J {
     public RiotApi() {
         super(new APICredentials(ConfigLoader.getConfig().getRiotToken()));
+        DataCall.setCacheProvider(EmptyCacheProvider.INSTANCE);
     }
 }
